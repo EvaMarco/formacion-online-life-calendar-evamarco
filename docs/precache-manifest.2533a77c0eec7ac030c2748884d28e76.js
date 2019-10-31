e829d0eeb166eb6bc88ae9deda321f3ff4ba1356
Migara <evamarcod@gmail.com>
2019-10-31 11:17:52 +0100

    Refresh docs, delete a duplicated link

---
+++ docs/precache-manifest.2533a77c0eec7ac030c2748884d28e76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae8d77f7c3afe8f31e98506ca2fe680e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "63532928b5b6a1a88429",
+    "url": "./static/css/main.e2d39c78.chunk.css"
+  },
+  {
+    "revision": "6dcd327245903fccf77a",
+    "url": "./static/js/2.3df9da34.chunk.js"
+  },
+  {
+    "revision": "63532928b5b6a1a88429",
+    "url": "./static/js/main.df24954b.chunk.js"
+  },
+  {
+    "revision": "f4d802c895a75b363dcf",
+    "url": "./static/js/runtime-main.c4b91c0e.js"
+  },
+  {
+    "revision": "ac603318fa06e308b0ab94f6cfc34aaf",
+    "url": "./static/media/fondoHeader.ac603318.jpg"
+  }
+]);
