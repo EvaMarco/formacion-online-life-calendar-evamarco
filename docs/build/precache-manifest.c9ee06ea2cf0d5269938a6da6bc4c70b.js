bc77621acc26a38c4627235c83d47410258275aa
Migara <evamarcod@gmail.com>
2019-10-31 10:39:47 +0100

    Add new docs folder

---
+++ docs/build/precache-manifest.c9ee06ea2cf0d5269938a6da6bc4c70b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70415d144106bd9bfdad24dd3fa7f197",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4a48259e427de5e726c4",
+    "url": "./static/css/main.e2d39c78.chunk.css"
+  },
+  {
+    "revision": "49f6250477f9b89c004a",
+    "url": "./static/js/2.b11f1078.chunk.js"
+  },
+  {
+    "revision": "4a48259e427de5e726c4",
+    "url": "./static/js/main.0c305cb2.chunk.js"
+  },
+  {
+    "revision": "f4d802c895a75b363dcf",
+    "url": "./static/js/runtime-main.c4b91c0e.js"
+  },
+  {
+    "revision": "ac603318fa06e308b0ab94f6cfc34aaf",
+    "url": "./static/media/fondoHeader.ac603318.jpg"
+  }
+]);
